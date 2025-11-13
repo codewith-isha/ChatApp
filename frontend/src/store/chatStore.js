3a8395d5a8143fceed6bc299349fddfe806dcb2f
gauravvgithub <gauravraj7011@gmail.com>
2025-11-13 17:21:49 +0000

    chatStore.js working

--- frontend/src/store/chatStore.js
+++ frontend/src/store/chatStore.js
@@ -1,99 +1,246 @@
 import { create } from "zustand";
+import { getSocket } from "../services/chat.service";
+import axiosInstance from "../services/url.service";
 
-export const useChatStore = create((set,get)=>({
-  conversations : [],
-  currrentConversation:null,
-  message:[],
-  loading:false,
-  error:null,
-  onlineUsers:new Map(),
-  typingUsers:new Map(),
+export const useChatStore = create((set, get) => ({
+  conversations: [],
+  currrentConversation: null,
+  message: [],
+  loading: false,
+  error: null,
+  onlineUsers: new Map(),
+  typingUsers: new Map(),
 
-  // socket event listners setup 
-  initsocketListners : () =>{
+  // socket event listners setup
+  initsocketListners: () => {
     const socket = getSocket();
-    if(!socket) return;
-    // remove exiting listerners to prevent duplicate hnadler 
-    socket.off("receive_message")
-    socket.off("user_typing")
-    socket.off("user_status")
-    socket.off("message_send")
-    socket.off("message_error")
-    socket.off("message_deleted")
+    if (!socket) return;
+    // remove exiting listerners to prevent duplicate hnadler
+    socket.off("receive_message");
+    socket.off("user_typing");
+    socket.off("user_status");
+    socket.off("message_send");
+    socket.off("message_error");
+    socket.off("message_deleted");
 
-    // liten for incoming message 
-    socket.on('receive_message',(message)=>{
+    // liten for incoming message
+    socket.on("receive_message", (message) => {});
 
+    socket.on("message_send", (message) => {
+      set((state) => ({
+        messages: state.messages.map((msg) =>
+          msg._id === message._id ? { ...msg } : msg
+        ),
+      }));
     });
 
-     socket.on('message_send',(message)=>{
-       set((state)=>({
-        messages:state.messages.map((msg)=>
-          msg._id === message._id ? {...msg} :msg
-        )
-       }))
+    socket.on("message_status_update", ({ messageId, messageStatus }) => {
+      set((state) => ({
+        messages: state.messages.map((map) =>
+          msg._id === messageId ? { ...msg, messageStatus } : msg
+        ),
+      }));
     });
 
-    socket.on("message_status_update",({messageId,messageStatus})=>{
-      set((state)=>({
-        messages:state.messages.map((map)=>
-        msg._id=== messageId ? {...msg,messageStatus}:msg)
-      }))
-    })
+    socket.on("reaction_update", ({ messageId, reactions }) => {
+      set((state) => ({
+        messages: state.messages.map((map) =>
+          msg._id === messageId ? { ...msg, reactions } : msg
+        ),
+      }));
+    });
 
-      socket.on("reaction_update",({messageId,reactions})=>{
-      set((state)=>({
-        messages:state.messages.map((map)=>
-        msg._id=== messageId ? {...msg,reactions}:msg)
-      }))
-    })
+    // handle delete message from local state
+    socket.on("message_deleted", ({ deletedMessageId }) => {
+      set((state) => ({
+        messages: state.messages.filter((map) => msg._id !== deletedMessageId),
+      }));
+    });
 
-    // handle delete message from local state 
-      socket.on("message_deleted",({deletedMessageId})=>{
-      set((state)=>({
-        messages:state.messages.filter((map)=>msg._id !== deletedMessageId)
-      }))
-    })
- 
     // handle any message sending error
-    socket.on("message_error",(error)=>{
-      console.error('message error', error)
-    })
-;
-socket.on("user_typing",({userId,conversationId,isTyping})=>{
-  set((state)=>{
-    const newTypingUsers = new Map(state.typingUsers)
-    if(!newTypingUsers.has(conversationId)){
-      newTypingUsers.set(conversationId,new Set())
+    socket.on("message_error", (error) => {
+      console.error("message error", error);
+    });
+    socket.on("user_typing", ({ userId, conversationId, isTyping }) => {
+      set((state) => {
+        const newTypingUsers = new Map(state.typingUsers);
+        if (!newTypingUsers.has(conversationId)) {
+          newTypingUsers.set(conversationId, new Set());
+        }
+        const typingSet = newTypingUsers.get(conversationId);
+        if (isTyping) {
+          typingSet.add(userId);
+        } else {
+          typingSet.delete(userId);
+        }
+        return { typingUsers: newTypingUsers };
+      });
+    });
+
+    // track user online/offline status
+    socket.on("user_status", ({ userId, isOnline, lastSeen }) => {
+      set((state) => {
+        const newOnlineUsers = new Map(state.onlineUsers);
+        newOnlineUsers.set(userId, { isOnline, lastSeen });
+        return { onlineUsers: newOnlineUsers };
+      });
+    });
+
+    const { conversations } = get();
+    if (conversations?.data?.length > 0) {
+      conversations.data?.forEach((conv) => {
+        const otherUser = conv.participants.find(
+          (p) => p._id !== get().currentUser._id
+        );
+
+        if (otherUser._id) {
+          socket.emit("get_user_status", otherUser._id, (status) => {
+            set((state) => {});
+            const newOnlineUsers = new Map(state.onlineUsers);
+            newOnlineUsers.set(state.userId, {
+              isOnline: state.isOnline,
+              lastSeen: state.lastSeen,
+            });
+
+            return { onlineUsers: newOnlineUsers };
+          });
+        }
+      });
     }
-    const typingSet = newTypingUsers.get(conversationId);
-    if(isTyping){
-      typingSet.add(userId)
-    }else{
-      typingSet.delete(userId)
+  },
+
+  setCurrentUser: (user) => set({ currentUser: user }),
+
+  fetchConversation: async () => {
+    set({ loading: true, error: null });
+    try {
+      const { data } = await axiosInstance.get("/chats/conversations");
+      set({ conversations: data, loading: false }), get().initsocketListners();
+      return data;
+    } catch (error) {
+      set({
+        error: error?.response?.data?.message || error?.message,
+        loading: false,
+      });
+
+      return null;
     }
-    return {typingUsers:newTypingUsers}
-  })
-});
+  },
 
-// track user online/offline status 
-socket.on("user_status",({userId,isOnline,lastSeen})=>{
-  set((state)=>{
-    const newOnlineUsers = new Map(state.onlineUsers);
-    newOnlineUsers.set(userId,{isOnline,lastSeen});
-    return {onlineUsers:newOnlineUsers}
-  })
-})
+  // fetch message for a conversations
 
-const {conversations} = get();
-if(conversations?.data?.length >0){
- conversations.data?.forEach((conv)=>{
-  const otherUser = conv.participants.find(
-    (p)=> p._id !== get().currentUser._id 
-   )
- })
-}
+  fetchMessages: async (conversationId) => {
+    if (!conversationId) return;
 
+    set({ loading: true, error: null });
+    try {
+      const { data } = await axiosInstance.get(
+        `/chats/conversations/${conversationId}/messages`
+      );
 
-  }
-}))
+      const messageArray = data.data || data || [];
+
+      set({
+        message: messageArray,
+        currrentConversation: conversationId,
+        loading: false,
+      });
+
+      // mark as unread message as read
+
+      return messageArray;
+    } catch (error) {
+      set({
+        error: error?.response?.data?.message || error?.message,
+        loading: false,
+      });
+
+      return [];
+    }
+  },
+
+  // send message in real time
+  sendMessage: async (formData) => {},
+
+  receiveMessage: (message) => {
+    if (!message) return;
+
+    const { currrentConversation, currentUser, messages } = get();
+
+    const messageExits = message.some((msg) => msg._id === message._id);
+    if (messageExits) return;
+
+    if (message.conversation === currrentConversation) {
+      set((state) => {
+        {
+          messages: [...state.messages, message];
+        }
+      });
+    }
+
+    //update conversation preview and unread count
+    set((state) => {
+      const updateConversations = state.conversations?.data?.map((conv) => {
+        if (conv._id === message.conversation) {
+          return {
+            ...conv,
+            lastMessage: message,
+            unreadCount:
+              message?.receiver?._id === currentUser?._id
+                ? (conv.unreadCount || 0) + 1
+                : conv.unreadCount || 0,
+          };
+        }
+
+        return conv;
+      });
+
+      return {
+        conversations: {
+          ...state.conversations,
+          data: updateConversations,
+        },
+      };
+    });
+  },
+
+  // mark as read
+  markMessagesAsRead: async () => {
+    const { messages, currentUser } = get();
+
+    if (!messages.length || !currentUser) return;
+    const unreadIds = messages
+      .filter(
+        (msg) =>
+          msg.messageStatus !== "read" && msg.receiver?._id === currentUser?._id
+      )
+      .map((msg) => msg._id)
+      .filter(Boolean);
+    if (unreadIds.length === 0) return;
+
+    try {
+      const { data } = await axiosInstance.put("/chats/messages/read", {
+        messageId: unreadIds,
+      });
+
+      console.log("message mark as read", data)
+      set((state) => ({
+        messages: state.messages.map((msg) =>
+          unreadIds.includes(msg._id) ? { ...msg, messageStatus: "read" } : msg
+        ),
+      }));
+
+      const socket = getSocket();
+      if (socket) {
+        socket.emit("message_read", {
+          messageIds: unreadIds,
+          senderId: messages[0]?.sender?._id,
+        });
+      }
+    } catch (error) {
+      console.error("failed to mark message as read", error)
+    }
+  },
+
+  deleteMessage
+}));
